5246660535183eefc993be112dc6c809da9b88f1
kimtaeho83 <kimtaeho83@a382de72-e177-11dd-9059-71fac19e68c6>
2009-03-07 15:20:42 +0000

    ExamPanel 추가했어요

--- trunk/src/jcbt/main/ExamPanel.java
+++ trunk/src/jcbt/main/ExamPanel.java
@@ -1,61 +1,59 @@
 package jcbt.main;
 
 import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.Component;
+import java.awt.Container;
+import java.awt.Dimension;
 import java.awt.FlowLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.HashMap;
 
+import javax.swing.BorderFactory;
+import javax.swing.BoxLayout;
+import javax.swing.ButtonGroup;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
+import javax.swing.JRadioButton;
 import javax.swing.JTextField;
 
+import jcbt.db.CBTDao;
+import jcbt.dto.Examination;
+import jcbt.dto.Question;
+import jcbt.dto.SessionManager;
 /**
  * 시험 패널
  * @author Son Minho
  */
-public class ExamPanel extends CBTPanel {
-
-	//테스트용-----------------------------------
-	String strQuestion = "1. 테스트용 문제..";
-	String strExample1 = "보기 1";
-	String strExample2 = "보기 2";
-	String strExample3 = "보기 3";
-	String strExample4 = "보기 4";
-	String strExample5 = "보기 5";
-	//------------------------------------------
+public class ExamPanel extends CBTPanel implements ActionListener{
+	final protected String prevString = "PREV";
+	final protected String nextString = "NEXT";
+	final protected String gotoString = "GO TO";
 	
-	JPanel pnlTest = new JPanel(new BorderLayout());
-	JPanel pnlExample = new JPanel(new GridLayout(5, 1));
+	Examination exam = null;
 	
-	JLabel lblQuestion = new JLabel();
+	JPanel examPanel = null;
+	JPanel functionPanel = null;
 	
-	JPanel pnlExample1 = null;
-	JPanel pnlExample2 = null;
-	JPanel pnlExample3 = null;
-	JPanel pnlExample4 = null;
-	JPanel pnlExample5 = null;
+	JButton prevButton = null;
+	JButton nextButton = null;
+	JButton gotoButton = null;
 	
-	JLabel lblExample1 = null;
-	JLabel lblExample2 = null;
-	JLabel lblExample3 = null;
-	JLabel lblExample4 = null;
-	JLabel lblExample5 = null;
+	JLabel indexLabel = null;
 	
-	JCheckBox chk1 = new JCheckBox();
-	JCheckBox chk2 = new JCheckBox();
-	JCheckBox chk3 = new JCheckBox();
-	JCheckBox chk4 = new JCheckBox();
-	JCheckBox chk5 = new JCheckBox();
+	JTextField putIndexField = null;
 	
-	JButton btnPrev = new JButton("PREV");
-	JButton btnNext = new JButton("NEXT");
-	JButton btnGoto = new JButton("GOTO");
-	
-	JLabel lblCurPage = new JLabel();
-	JLabel lblPage = new JLabel("page");
-	
-	JTextField tfPage = new JTextField();
+	int currentQuestionNumber;
+	int numOfQuestion = 0;
 	
 	public ExamPanel(CBTMain cbtMain) {
 		super(cbtMain);
@@ -64,40 +62,173 @@ public class ExamPanel extends CBTPanel {
 	}
 	
 	public void init() {
-		lblQuestion.setText(strQuestion);
+		//exam = cbtMain.getExam();
 		
-		pnlExample1 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		pnlExample2 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		pnlExample3 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		pnlExample4 = new JPanel(new FlowLayout(FlowLayout.LEFT));
-		pnlExample5 = new JPanel(new FlowLayout(FlowLayout.LEFT));
+		CBTDao cbtDao = new CBTDao();
+		HashMap<String, ArrayList<String>> map = cbtDao.getExamList();
+		ArrayList<String> examID = (ArrayList<String>)map.get("examid");
+		exam = cbtDao.getExam(examID.get(0));
 		
-		lblExample1 = new JLabel(strExample1);
-		lblExample2 = new JLabel(strExample2);
-		lblExample3 = new JLabel(strExample3);
-		lblExample4 = new JLabel(strExample4);
-		lblExample5 = new JLabel(strExample5);
+		numOfQuestion = exam.getQuestionSize();
 		
-		pnlExample1.add(chk1);
-		pnlExample1.add(lblExample1);
-		pnlExample2.add(chk2);
-		pnlExample2.add(lblExample2);
-		pnlExample3.add(chk3);
-		pnlExample3.add(lblExample3);
-		pnlExample4.add(chk4);
-		pnlExample4.add(lblExample4);
-		pnlExample5.add(chk5);
-		pnlExample5.add(lblExample5);
+		// set Function Panel up
+		makeFunctionPanel();
 		
-		pnlExample.add(pnlExample1);
-		pnlExample.add(pnlExample2);
-		pnlExample.add(pnlExample3);
-		pnlExample.add(pnlExample4);
-		pnlExample.add(pnlExample5);
+		// set Exam Panel up
+		examPanel = new JPanel();
+		examPanel.setLayout(new BoxLayout(examPanel, BoxLayout.Y_AXIS));
+		examPanel.setBorder(BorderFactory.createLineBorder(Color.black));
+		showQuestion(1);
+	
+		add(functionPanel, BorderLayout.PAGE_END);
+		add(examPanel, BorderLayout.CENTER);
+	}
+	
+	private void makeFunctionPanel(){
+		functionPanel = new JPanel();
 		
-		pnlTest.add(lblQuestion, BorderLayout.NORTH);
-		pnlTest.add(pnlExample, BorderLayout.CENTER);
+		prevButton = new JButton(prevString);
+		nextButton = new JButton(nextString);
+		gotoButton = new JButton(gotoString);
 		
-		add(pnlTest, BorderLayout.CENTER);
+		indexLabel = new JLabel("test");
+		
+		putIndexField = new JTextField(2);
+		
+		// add action listener
+		ButtonActionHandler handler = new ButtonActionHandler();
+		prevButton.addActionListener(handler);
+		nextButton.addActionListener(handler);
+		gotoButton.addActionListener(handler);
+		
+		// set layout
+		GridBagLayout gridbag = new GridBagLayout();
+        functionPanel.setLayout(gridbag);
+        
+        GridBagConstraints c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 0;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
+        c.gridx = 0;
+        c.gridwidth = 1;
+        c.gridy = 0;
+        c.gridheight = 1;
+        functionPanel.add(prevButton, c);
+        
+        c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 0;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
+        c.gridx = 1;
+        c.gridwidth = 1;
+        c.gridy = 0;
+        c.gridheight = 1;
+        functionPanel.add(nextButton, c);
+        
+        c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 0;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
+        c.gridx = 2;
+        c.gridwidth = 1;
+        c.gridy = 0;
+        c.gridheight = 1;
+        functionPanel.add(indexLabel, c);
+        
+        c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 0;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
+        c.gridx = 3;
+        c.gridwidth = 1;
+        c.gridy = 0;
+        c.gridheight = 1;
+        functionPanel.add(gotoButton, c);
+        
+        c = new GridBagConstraints();
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 0;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
+        c.gridx = 4;
+        c.gridwidth = 1;
+        c.gridy = 0;
+        c.gridheight = 1;
+        functionPanel.add(putIndexField, c);
+        
+        c= new GridBagConstraints();
+        JLabel page = new JLabel("page");
+        c.fill = GridBagConstraints.HORIZONTAL;
+        c.ipady = 0;
+        c.weightx = 0.0;
+        c.weighty = 0.0;
+        c.gridx = 5;
+        c.gridwidth = 1;
+        c.gridy = 0;
+        c.gridheight = 1;
+        functionPanel.add(page, c);
+	}
+	
+	private void showQuestion(int index){
+		
+		//Question question = exam.getQuestion(index);
+		Question question = null;
+		if(question == null){
+			return;
+		}
+		currentQuestionNumber = index;
+		
+		int exampleSize = question.getExampleSize();
+		JLabel questionSentence = new JLabel();
+		questionSentence.setText(index+question.getQuestion());
+		
+		questionSentence.setAlignmentX(Component.LEFT_ALIGNMENT);
+		examPanel.add(questionSentence);
+		ButtonGroup group = new ButtonGroup();
+		for(int i=1; i<=exampleSize; i++){
+			addExample(i, question.getExample(i), group, examPanel);
+		}
+		
+		indexLabel.setText(currentQuestionNumber+"/"+numOfQuestion);
+	}
+	
+	private void addExample(int index, String content, ButtonGroup group, Container container){
+		JRadioButton radioButton = new JRadioButton(index+"."+content);
+		radioButton.setActionCommand(index+"");
+		radioButton.addActionListener(this);
+		
+		container.add(radioButton);
+		group.add(radioButton);
+	}
+
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		cbtMain.setSelectedAnswer(currentQuestionNumber, e.getActionCommand());
+	}
+	
+	class ButtonActionHandler implements ActionListener{
+
+		public void actionPerformed(ActionEvent e) {
+			// TODO Auto-generated method stub
+			if(e.getActionCommand().equals(prevString)){
+				showQuestion(currentQuestionNumber-1);
+			}else if(e.getActionCommand().equals(nextString)){
+				showQuestion(currentQuestionNumber+1);
+			}else if(e.getActionCommand().equals(gotoString)){
+				try{
+					int pageNum = Integer.parseInt(putIndexField.getText());
+					showQuestion(pageNum);
+				}catch(NumberFormatException nfe){
+				}finally{
+					putIndexField.setText("");
+				}
+			}else{
+			}
+		}
+		
 	}
 }
